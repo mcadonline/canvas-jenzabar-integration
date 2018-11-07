7b1deb540486c4fcb235bb6fa1e53fc4f06143bc
James Johnson <johnsojr@gmail.com>
2018-11-07 16:26:50 -0600

    Get faculty and students from jex

---
+++ src/services/jex/index.js
@@ -0,0 +1,15 @@
+import SQLService from '../SQLService';
+import getStudentsFromJex from './getStudentsFromJex';
+import getInstructorsFromJex from './getInstructorsFromJex';
+
+const jexService = new SQLService('jex');
+
+export default {
+  async getUsers() {
+    const [students, faculty] = await Promise.all([
+      getStudentsFromJex(jexService),
+      getInstructorsFromJex(jexService),
+    ]);
+    return students.concat(faculty);
+  },
+};
