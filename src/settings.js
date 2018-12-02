8ba4779ed161443d8020673a51e3c3a9f001e790
James Johnson <johnsojr@gmail.com>
2018-12-02 11:25:20 -0600

    Add additional pilot courses.

--- src/settings.js
+++ src/settings.js
@@ -24,7 +24,16 @@ const settings = {
 
   // users and enrollments will only be processed for these courses
   // (course id must be in JEX format)
-  onlyCourses: ['IDM  6611 20', 'SD   6750 20', 'GWD  7460 20', 'HS   5010 20', '2D   3206 20'],
+  onlyCourses: [
+    'IDM  6611 20',
+    'SD   6750 20',
+    'GWD  7460 20',
+    'HS   5010 20',
+    '2D   3206 20',
+    'ILL  2000 01',
+    'GRD  2000 01',
+    'AH   2103 01',
+  ],
   onlyTerm: 'SP',
   onlyRealYear: 2019,
 
