f955531dc95a998c9c7cbc939b497d7da88501ef
James Johnson <johnsojr@gmail.com>
2019-01-19 22:32:04 -0600

    post data to canvas

--- src/services/canvas/index.js
+++ src/services/canvas/index.js
@@ -1,9 +1,11 @@
 import getUsersFromCanvas from './getUsersFromCanvas';
 import getCoursesFromCanvas from './getCoursesFromCanvas';
 import getStudentEnrollmentFromCanvas from './getStudentEnrollmentFromCanvas';
+import postToCanvas from './postToCanvas';
 
 export default {
   getUsers: getUsersFromCanvas,
   getCourses: getCoursesFromCanvas,
   getStudentEnrollment: getStudentEnrollmentFromCanvas,
+  post: postToCanvas,
 };
