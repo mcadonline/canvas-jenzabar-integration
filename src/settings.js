d5f0f7c8dd637c12bbafdbb6f661424a613c9b97
James Johnson <johnsojr@gmail.com>
2019-06-05 12:50:09 -0500

    Add Fall 2019 Canvas Courses

--- src/settings.js
+++ src/settings.js
@@ -40,7 +40,26 @@ const settings = {
     {
       term: 'FA',
       year: 2019,
-      sections: ['GWD  7410 20', 'CSDE 9478 20', 'GWD  7460 20', 'HS   5010 20'],
+      sections: [
+        'GWD  7800 20', // MAGWD / Capstone
+        'GWD  7410 20', // MAGWD / Web Design
+        'CSDE 9478 20', // CE / Motion Illustration
+        'GWD  7460 20', // MAGWD / UX Design
+        'HS   5010 20', // LA / LA Adv Sem
+        'GWD  6610 20', // MAGWD / Web Dev: HTML + CSS
+        'IDM  6611 20', // Web Dev: HTML
+        'IDM  6612 20', // Web Dev: CSS
+        'IDM  6613 20', // Web Dev: Projects
+        'SD   7010 20', // SD Practicum
+        'SD   7020 20', // SD Thesis Project
+        'CSDE 9305 20', // CE / Comic Art Crash Course
+        'CSID 6631 20', // Web Dev: PHP and MySQL
+        'HS   3317 20', // LA / Myth, Ritual, and Symbolism
+        'SD   6510 20', // SD / Systems Thinking
+        'VC   4207 20', // CE / Graphic Design Essentials
+        'CSID 6632 20', // CE / Web Dev: Wordpress
+        'CSMA 9342 20', // CE / Game Mechanics
+      ],
     },
   ],
 
