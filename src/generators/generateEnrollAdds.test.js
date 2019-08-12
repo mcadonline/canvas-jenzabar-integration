48c943828bfef31ea4d0eb67aca010addc514d55
James Johnson <johnsojr@gmail.com>
2019-08-12 09:46:55 -0500

    remove old tests and add todos for new ones

--- src/generators/generateEnrollAdds.test.js
+++ src/generators/generateEnrollAdds.test.js
@@ -1,36 +1,5 @@
-import generateEnrollAdds from './generateEnrollAdds';
+// import generateEnrollAdds from './generateEnrollAdds';
 
 describe('generateEnrollAdds', () => {
-  it('gets a list of new enrollments to add to Canvas', async () => {
-    const jexService = {
-      getStudentEnrollment: jest.fn().mockResolvedValue([
-        {
-          course_id: 'Art 101',
-          user_id: 'enrolled user',
-        },
-        {
-          course_id: 'Art 101',
-          user_id: 'new user',
-        },
-      ]),
-    };
-
-    const canvasService = {
-      getStudentEnrollment: jest
-        .fn()
-        .mockResolvedValue([{ course_id: 'Art 101', user_id: 'enrolled user' }]),
-    };
-
-    const expected = [
-      '"course_id","user_id","status","role"',
-      '"Art 101","new user","active","student"',
-    ].join('\n');
-
-    const csv = await generateEnrollAdds({
-      jex: jexService,
-      canvas: canvasService,
-    });
-
-    expect(csv).toEqual(expected);
-  });
+  it.todo('gets a list of new enrollments to add to Canvas');
 });
---
+++ src/generators/generateUsers.test.js
@@ -0,0 +1,5 @@
+describe('generateUsers', () => {
+  it.todo('creates a list of new users to add to Canvas');
+  it.todo('includes users who need updates (e.g. last name change)');
+  it.todo('includes faculty users');
+});
