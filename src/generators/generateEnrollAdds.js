91921ce5504d2f55bf797a7fecb4a0d6a0003e60
James Johnson <johnsojr@gmail.com>
2019-08-11 22:19:05 -0500

    generate enrollment adds

--- src/generators/generateEnrollAdds.js
+++ src/generators/generateEnrollAdds.js
@@ -1,16 +1,37 @@
+import canvas from '../services/canvas';
+import jex from '../services/jex';
 import setMinus from '../utils/setMinus';
+import toCourseId from '../utils/toCourseId';
 import jsonToCSV from '../utils/jsonToCSV';
 
-export default async ({ jex, canvas }) => {
-  const [currentJexStudentEnrollments, currentCanvasStudentEnrollments] = await Promise.all([
-    jex.getStudentEnrollment(),
+export default async () => {
+  const [activeCanvasSections, canvasEnrollment, jexEnrollment] = await Promise.all([
+    canvas.getActiveSections(),
     canvas.getStudentEnrollment(),
+    jex.getStudentEnrollment(),
   ]);
 
-  const newCanvasEnrollments = setMinus(
-    currentJexStudentEnrollments,
-    currentCanvasStudentEnrollments,
-  ).map(enrollee => ({ ...enrollee, status: 'active', role: 'student' }));
+  // createa lookup table so that we can see which
+  // of our jex students may be in an active canvas section
+  const activeSectionIdLookup = activeCanvasSections
+    .map(s => s.sis_section_id)
+    .reduce((acc, id) => ({ ...acc, [id]: true }), {});
 
-  return jsonToCSV(newCanvasEnrollments);
+  const jexEnrolleesWithActiveCanvasSections = jexEnrollment.filter(
+    ({ courseCode, term, year }) => {
+      const sectionId = toCourseId({ courseCode, term, year });
+      const isInActiveSection = !!activeSectionIdLookup[sectionId];
+      return isInActiveSection;
+    },
+  );
+
+  // These are the enrollments which SHOULD be in Canvas
+  const sisEnrollmentsFromJex = jex.toSisEnrollment(jexEnrolleesWithActiveCanvasSections);
+
+  // Enrollments which currently ARE in Canvas
+  const sisEnrollmentsFromCanvas = canvas.toSisEnrollment(canvasEnrollment);
+
+  const enrollmentsToAdd = setMinus(sisEnrollmentsFromJex, sisEnrollmentsFromCanvas);
+
+  return jsonToCSV(enrollmentsToAdd);
 };
