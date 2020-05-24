3a92451c21a0a8b63cc329f29810f404196dddeb
James Johnson <james_johnson@mcad.edu>
2020-05-23 19:26:22 -0500

    sandbox names are preferredname lastname

--- src/generators/generateSandboxes.js
+++ src/generators/generateSandboxes.js
@@ -1,10 +1,17 @@
 /* eslint-disable camelcase */
-import { uniq, difference } from 'ramda';
+import { uniq, differenceWith } from 'ramda';
 import jsonToCSV from '../utils/jsonToCSV';
 import jex from '../services/jex';
 import canvas from '../services/canvas';
 
-const getOnlineWorkshopAttendeeUsernames = async () => {
+const convertToUser = ({ firstName, preferredName, lastName, username }) => ({
+  firstName,
+  preferredName,
+  lastName,
+  username,
+});
+
+const getOnlineWorkshopAttendeeUsers = async () => {
   // get all students enrolled in current and upcoming courses
   // then identify only those students enrolled in a workshop like
   // OL-0xxx.
@@ -13,32 +20,30 @@ const getOnlineWorkshopAttendeeUsernames = async () => {
   const workshopAttendeesWithUsername = allStudentEnrollment.filter(
     ({ courseCode, username }) => olWorkshopCourseCodeRegEx.test(courseCode) && !!username
   );
-  return workshopAttendeesWithUsername.map(attendee => attendee.username);
+  return workshopAttendeesWithUsername.map(convertToUser);
 };
 
-const getInstructorUsernames = async () => {
+const getInstructorUsers = async () => {
   const instructors = await jex.getInstructors();
-  return instructors
-    .filter(instructor => !!instructor.username)
-    .map(instructor => instructor.username);
+  return instructors.filter(instructor => !!instructor.username).map(convertToUser);
 };
 
 /**
  * gets a list of all instructors (of current and future courses)
  * and all enrollees in online learning workshops like OL-0xxx
  */
-const getUsernamesWhoShouldHaveSandbox = async () => {
-  const [onlineWorkshopAttendeeUsernames, instructorUsernames] = await Promise.all([
-    getOnlineWorkshopAttendeeUsernames(),
-    getInstructorUsernames(),
+const getUsersWhoShouldHaveSandbox = async () => {
+  const [onlineWorkshopAttendeeUsers, instructorUsers] = await Promise.all([
+    getOnlineWorkshopAttendeeUsers(),
+    getInstructorUsers(),
   ]);
 
   // deduplicate usernames.
   // Instructors may be workshop attendees and vice versa.
-  return uniq([...onlineWorkshopAttendeeUsernames, ...instructorUsernames]);
+  return uniq([...onlineWorkshopAttendeeUsers, ...instructorUsers]);
 };
 
-const getUsernamesWhoAlreadyHasSandbox = async () => {
+const getUsernamesWhoAlreadyHaveSandbox = async () => {
   const coursesInCanvas = await canvas.getCourses();
   return coursesInCanvas
     .filter(({ sis_course_id }) => /^SANDBOX-.+/.test(sis_course_id))
@@ -54,25 +59,30 @@ const getUsernamesWhoAlreadyHasSandbox = async () => {
 export default async () => {
   // get a list of all usernames of the peeps
   // who need a sandbox in Canvas
-  const [shouldHaveSandboxUsernames, alreadyHasSandboxUsernames] = await Promise.all([
-    getUsernamesWhoShouldHaveSandbox(),
-    getUsernamesWhoAlreadyHasSandbox(),
+  const [shouldHaveSandboxUsers, alreadyHasSandboxUsernames] = await Promise.all([
+    getUsersWhoShouldHaveSandbox(),
+    getUsernamesWhoAlreadyHaveSandbox(),
   ]);
 
+  const userHasUsernameInList = (user, username) => user.username === username;
+
   // who doesn't have a sandbox that needs one?
-  const needsNewSandboxUsernames = difference(
-    shouldHaveSandboxUsernames,
+  const usersNeedingNewSandbox = differenceWith(
+    userHasUsernameInList,
+    shouldHaveSandboxUsers,
     alreadyHasSandboxUsernames
   );
 
   // only parent courses should have a course shell
-  const canvasCsvCourses = needsNewSandboxUsernames.map(username => ({
-    course_id: `SANDBOX-${username}`.toUpperCase(),
-    short_name: `SANDBOX-${username}`.toUpperCase(),
-    long_name: `SANDBOX: ${username.toUpperCase()}`,
-    status: 'active',
-    blueprint_course_id: 'TEMPLATE-ENHANCEDCOURSE',
-  }));
+  const canvasCsvCourses = usersNeedingNewSandbox.map(
+    ({ firstName, preferredName, lastName, username }) => ({
+      course_id: `SANDBOX-${username}`.toUpperCase(),
+      short_name: `SANDBOX-${username}`.toUpperCase(),
+      long_name: `SANDBOX: ${preferredName || firstName} ${lastName}`,
+      status: 'active',
+      blueprint_course_id: 'TEMPLATE-ENHANCEDCOURSE',
+    })
+  );
 
   const csv = jsonToCSV(canvasCsvCourses);
   return csv;
--- src/generators/generateSandboxes.test.js
+++ src/generators/generateSandboxes.test.js
@@ -3,13 +3,8 @@ import jex from '../services/jex';
 import canvas from '../services/canvas';
 
 describe('generateSandboxes', () => {
-  it('matches CanvasCSV format', async () => {
-    const csv = await generateSandboxes();
-    const firstLine = csv.split('\n')[0];
-    expect(firstLine).toMatchInlineSnapshot(`
-      "course_id,short_name,long_name,status,blueprint_course_id
-      "
-    `);
+  afterEach(() => {
+    jest.restoreAllMocks();
   });
 
   it('creates a sandbox for every current and future faculty people', async () => {
@@ -44,20 +39,187 @@ describe('generateSandboxes', () => {
     const csv = await generateSandboxes();
     expect(csv).toMatchInlineSnapshot(`
       "course_id,short_name,long_name,status,blueprint_course_id
-      SANDBOX-EBENES,SANDBOX-EBENES,SANDBOX: EBENES,active,TEMPLATE-ENHANCEDCOURSE
-      SANDBOX-GCOSTANZA,SANDBOX-GCOSTANZA,SANDBOX: GCOSTANZA,active,TEMPLATE-ENHANCEDCOURSE"
+      SANDBOX-EBENES,SANDBOX-EBENES,SANDBOX: Laney Benes,active,TEMPLATE-ENHANCEDCOURSE
+      SANDBOX-GCOSTANZA,SANDBOX-GCOSTANZA,SANDBOX: George Costanza,active,TEMPLATE-ENHANCEDCOURSE"
     `);
   });
 
-  it.todo('creates a sandbox for every person enrolled in OL workshop courses');
+  it('creates a sandbox for every person enrolled in OL workshops (courses like OL-0xxx)', async () => {
+    jest.spyOn(jex, 'getInstructors');
+    jex.getInstructors.mockResolvedValue([]);
 
-  it.todo(
-    'doesnt have duplicates, for instance if an instructor is also enrolled in a faculty workshop'
-  );
+    jest.spyOn(jex, 'getStudentEnrollment');
+    jex.getStudentEnrollment.mockResolvedValue([
+      {
+        id: 1,
+        firstName: 'Elaine',
+        preferredName: 'Laney',
+        lastName: 'Benes',
+        mcadEmail: 'ebenes@mcad.edu',
+        username: 'ebenes',
+        courseCode: 'OL   0110 20',
+        term: 'FA',
+        year: 2020,
+      },
+      {
+        id: 2,
+        firstName: 'George',
+        preferredName: null,
+        lastName: 'Costanza',
+        mcadEmail: 'gcostanza@mcad.edu',
+        username: 'gcostanza',
+        // non-workshop course participatnts shouldn't have sandbox.
+        courseCode: 'OL   1000 20',
+        term: 'FA',
+        year: 2020,
+      },
+    ]);
 
-  it.todo('doesnt create a sandbox if one already exists in Canvas');
+    jest.spyOn(canvas, 'getCourses');
+    canvas.getCourses.mockResolvedValue([]);
+    const csv = await generateSandboxes();
+    expect(csv).toMatchInlineSnapshot(`
+      "course_id,short_name,long_name,status,blueprint_course_id
+      SANDBOX-EBENES,SANDBOX-EBENES,SANDBOX: Laney Benes,active,TEMPLATE-ENHANCEDCOURSE"
+    `);
+  });
 
-  it.todo('create a sandbox with course id like SANDBOX-USERNAME');
+  it('doesnt have duplicates, for instance if an instructor is also enrolled in a faculty workshop', async () => {
+    jest.spyOn(jex, 'getInstructors');
+    jex.getInstructors.mockResolvedValue([
+      {
+        id: 1,
+        firstName: 'Elaine',
+        preferredName: 'Laney',
+        lastName: 'Benes',
+        personalEmail: 'elaine@jpederman.com',
+        mcadEmail: 'ebenes@mcad.edu',
+        username: 'ebenes',
+      },
+    ]);
 
-  it.todo('creates a sandbox with long name like SANDBOX: Firstname Lastname');
+    jest.spyOn(jex, 'getStudentEnrollment');
+    jex.getStudentEnrollment.mockResolvedValue([
+      {
+        id: 1,
+        firstName: 'Elaine',
+        preferredName: 'Laney',
+        lastName: 'Benes',
+        mcadEmail: 'ebenes@mcad.edu',
+        username: 'ebenes',
+        courseCode: 'OL   0110 20',
+        term: 'FA',
+        year: 2020,
+      },
+    ]);
+
+    jest.spyOn(canvas, 'getCourses');
+    canvas.getCourses.mockResolvedValue([]);
+
+    const csv = await generateSandboxes();
+    expect(csv).toMatchInlineSnapshot(`
+      "course_id,short_name,long_name,status,blueprint_course_id
+      SANDBOX-EBENES,SANDBOX-EBENES,SANDBOX: Laney Benes,active,TEMPLATE-ENHANCEDCOURSE"
+    `);
+  });
+
+  it('doesnt create a sandbox if one already exists in Canvas', async () => {
+    jest.spyOn(jex, 'getInstructors');
+    jex.getInstructors.mockResolvedValue([
+      // pretend that this user already has a sandbox
+      {
+        id: 1,
+        firstName: 'Elaine',
+        preferredName: 'Laney',
+        lastName: 'Benes',
+        personalEmail: 'elaine@jpederman.com',
+        mcadEmail: 'ebenes@mcad.edu',
+        username: 'ebenes',
+      },
+      // pretend that this user does not have a sandbox
+      {
+        id: 2,
+        firstName: 'George',
+        preferredName: null,
+        lastName: 'Costanza',
+        personalEmail: 'art@vandelayindustries.com',
+        mcadEmail: 'gcostanza@mcad.edu',
+        username: 'gcostanza',
+      },
+    ]);
+
+    jest.spyOn(jex, 'getStudentEnrollment');
+    jex.getStudentEnrollment.mockResolvedValue([]);
+
+    jest.spyOn(canvas, 'getCourses');
+    canvas.getCourses.mockResolvedValue([
+      {
+        id: 123,
+        sis_course_id: 'SANDBOX-EBENES',
+        course_code: 'SANDBOX-EBENES',
+        name: 'SANDBOX: Elaine Benes',
+        status: 'available',
+        start_date: null,
+        end_date: null,
+        course_format: 'online',
+        total_students: 0,
+      },
+    ]);
+
+    const csv = await generateSandboxes();
+    expect(csv).toMatchInlineSnapshot(`
+      "course_id,short_name,long_name,status,blueprint_course_id
+      SANDBOX-GCOSTANZA,SANDBOX-GCOSTANZA,SANDBOX: George Costanza,active,TEMPLATE-ENHANCEDCOURSE"
+    `);
+  });
+
+  it('create a sandbox with course id like SANDBOX-USERNAME', async () => {
+    jest.spyOn(jex, 'getInstructors');
+    jex.getInstructors.mockResolvedValue([
+      {
+        id: 1,
+        firstName: 'First',
+        preferredName: 'Preferred',
+        lastName: 'Last',
+        username: 'username',
+      },
+    ]);
+
+    jest.spyOn(jex, 'getStudentEnrollment');
+    jex.getStudentEnrollment.mockResolvedValue([]);
+
+    jest.spyOn(canvas, 'getCourses');
+    canvas.getCourses.mockResolvedValue([]);
+
+    const csv = await generateSandboxes();
+    expect(csv).toMatchInlineSnapshot(`
+      "course_id,short_name,long_name,status,blueprint_course_id
+      SANDBOX-USERNAME,SANDBOX-USERNAME,SANDBOX: Preferred Last,active,TEMPLATE-ENHANCEDCOURSE"
+    `);
+  });
+
+  it('creates a sandbox with long name like SANDBOX: PreferredName Lastname', async () => {
+    jest.spyOn(jex, 'getInstructors');
+    jex.getInstructors.mockResolvedValue([
+      {
+        id: 1,
+        firstName: 'FirstName',
+        preferredName: 'PreferredName',
+        lastName: 'LastName',
+        username: 'username',
+      },
+    ]);
+
+    jest.spyOn(jex, 'getStudentEnrollment');
+    jex.getStudentEnrollment.mockResolvedValue([]);
+
+    jest.spyOn(canvas, 'getCourses');
+    canvas.getCourses.mockResolvedValue([]);
+
+    const csv = await generateSandboxes();
+    expect(csv).toMatchInlineSnapshot(`
+      "course_id,short_name,long_name,status,blueprint_course_id
+      SANDBOX-USERNAME,SANDBOX-USERNAME,SANDBOX: PreferredName LastName,active,TEMPLATE-ENHANCEDCOURSE"
+    `);
+  });
 });
