82afab5f2a5773287105e95955e00847496ba897
James Johnson <james_johnson@mcad.edu>
2020-05-23 21:24:00 -0500

    generate sandbox enrollments

--- src/generators/generateSandboxes.js
+++ src/generators/generateSandboxes.js
@@ -1,47 +1,8 @@
 /* eslint-disable camelcase */
-import { uniq, differenceWith } from 'ramda';
+import { differenceWith } from 'ramda';
 import jsonToCSV from '../utils/jsonToCSV';
-import jex from '../services/jex';
 import canvas from '../services/canvas';
-
-const convertToUser = ({ firstName, preferredName, lastName, username }) => ({
-  firstName,
-  preferredName,
-  lastName,
-  username,
-});
-
-const getOnlineWorkshopAttendeeUsers = async () => {
-  // get all students enrolled in current and upcoming courses
-  // then identify only those students enrolled in a workshop like
-  // OL-0xxx.
-  const allStudentEnrollment = await jex.getStudentEnrollment();
-  const olWorkshopCourseCodeRegEx = /^OL\s+0\d{3} \w+/;
-  const workshopAttendeesWithUsername = allStudentEnrollment.filter(
-    ({ courseCode, username }) => olWorkshopCourseCodeRegEx.test(courseCode) && !!username
-  );
-  return workshopAttendeesWithUsername.map(convertToUser);
-};
-
-const getInstructorUsers = async () => {
-  const instructors = await jex.getInstructors();
-  return instructors.filter(instructor => !!instructor.username).map(convertToUser);
-};
-
-/**
- * gets a list of all instructors (of current and future courses)
- * and all enrollees in online learning workshops like OL-0xxx
- */
-const getUsersWhoShouldHaveSandbox = async () => {
-  const [onlineWorkshopAttendeeUsers, instructorUsers] = await Promise.all([
-    getOnlineWorkshopAttendeeUsers(),
-    getInstructorUsers(),
-  ]);
-
-  // deduplicate usernames.
-  // Instructors may be workshop attendees and vice versa.
-  return uniq([...onlineWorkshopAttendeeUsers, ...instructorUsers]);
-};
+import getUsersWhoShouldHaveSandbox from '../utils/getUsersWhoShouldHaveSandbox';
 
 const getUsernamesWhoAlreadyHaveSandbox = async () => {
   const coursesInCanvas = await canvas.getCourses();
