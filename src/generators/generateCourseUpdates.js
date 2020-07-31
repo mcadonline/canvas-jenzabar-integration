0719c462e210f5f1c673168128ebb36c8885b78b
James Johnson <james_johnson@mcad.edu>
2020-07-31 16:16:31 -0500

    check if courses need updating WIP

--- src/generators/generateCourseUpdates.js
+++ src/generators/generateCourseUpdates.js
@@ -1,1 +1,115 @@
-export default async () => {};
+import { intersection, indexBy, keys, equals } from 'ramda';
+import { DateTime } from 'luxon';
+import jsonToCsv from '../utils/jsonToCSV';
+import jex from '../services/jex';
+import canvas from '../services/canvas';
+
+/**
+ * Converts a course to an object with keys equal to headers of
+ * Canvas CSV format.
+ *
+ * @link https://canvas.instructure.com/doc/api/file.sis_csv.html
+ *
+ * @param {Object} jexCourse - the course to convert in jex format
+ * @param {string} currentDateTimeISO  – current date time. If past the
+ * open datetime, then the course will published.
+ */
+const jexCourseToCanvasCsvFormat = (jexCourse, currentDateTimeISO) => {
+  if (!jexCourse) throw Error(`missing required argument jexCourse`);
+  if (!currentDateTimeISO) throw Error(`missing required argument currentDateTimeISO`);
+
+  // should this course be published?
+  const now = DateTime.fromISO(currentDateTimeISO);
+  const openDate = DateTime.fromISO(jexCourse.openDate);
+  const shouldBePublished = now >= openDate;
+
+  return {
+    course_id: jexCourse.id,
+    short_name: jexCourse.id,
+    long_name: jexCourse.name,
+    // See TODO in canvasApiCourseToCanvasCsvFormat
+    // term_id: `${jexCourse.year}-${jexCourse.term}`,
+    status: shouldBePublished ? 'published' : 'active',
+    start_date: jexCourse.openDate,
+    end_date: jexCourse.closeDate,
+  };
+};
+
+const workflowStateToCanvasCsvStatus = workflowState => {
+  switch (workflowState) {
+    case 'unpublished':
+      return 'active';
+    case 'available':
+      return 'published';
+    case 'completed':
+      return 'completed';
+    case 'deleted':
+      return 'deleted';
+    default:
+      return workflowState;
+  }
+};
+
+const canvasApiCourseToCanvasCsvFormat = canvasCourse => {
+  if (!canvasCourse) throw Error(`missing required argument: canvasCourse`);
+
+  return {
+    course_id: canvasCourse.sis_course_id,
+    short_name: canvasCourse.course_code,
+    long_name: canvasCourse.name,
+    // TODO:
+    // need to include[]=term in Canvas API call
+    // if we want to get ti term ids.
+    // term_id: canvasCourse.term.sis_term_id,
+    status: workflowStateToCanvasCsvStatus(canvasCourse.workflow_state),
+    start_date: canvasCourse.start_at,
+    end_date: canvasCourse.end_at,
+  };
+};
+
+/**
+ * generates a list of canvas courses to update in Canvas CSV SIS upload format,
+ * checking name, startDate, endDate, publish state against the SIS.
+ *
+ * @param {Object} opts - options
+ * @param {string} [opt.today=now] - the curent date time in ISO
+ * format. Defaults to current datetime.  Used to decide whether the course
+ * should be published or not
+ *
+ * @returns {Object[]} - a list of courses in Canvas CSV format
+ */
+export default async ({ today = DateTime.local().toISO() } = {}) => {
+  const [jexCourses, canvasCourses] = await Promise.all([
+    jex.getActiveCourses(),
+    canvas.getCourses(),
+  ]);
+
+  const jexCoursesIndexedById = indexBy(c => c.id, jexCourses);
+  const canvasCoursesIndexedById = indexBy(c => c.sis_course_id, canvasCourses);
+
+  // compare the courseIds in the list of jexCourses with the
+  // courseIds in canvasCourses. Get a list of courseIds that
+  // appear in both lists. These are the ids of courses to reconcile.
+  const courseIdsInBoth = intersection(keys(jexCoursesIndexedById), keys(canvasCoursesIndexedById));
+
+  const coursesToUpdate = courseIdsInBoth.reduce((acc, courseId) => {
+    // get both courses and convert both courses to common format
+    const jexCourseInCsvFormat = jexCourseToCanvasCsvFormat(jexCoursesIndexedById[courseId], today);
+    const canvasCourseInCsvFormat = canvasApiCourseToCanvasCsvFormat(
+      canvasCoursesIndexedById[courseId]
+    );
+
+    const isUpToDate = equals(jexCourseInCsvFormat, canvasCourseInCsvFormat);
+
+    return isUpToDate
+      ? acc
+      : [
+          ...acc,
+          // jex is our source of truth
+          // so this course represents what should be
+          jexCourseInCsvFormat,
+        ];
+  }, []);
+
+  return jsonToCsv(coursesToUpdate);
+};
--- src/generators/generateCourseUpdates.test.js
+++ src/generators/generateCourseUpdates.test.js
@@ -1,6 +1,102 @@
-describe('generateCourses', () => {
+import jex from '../services/jex';
+import canvas from '../services/canvas';
+import generateCourseUpdates from './generateCourseUpdates';
+
+describe('generateCourseUpdates', () => {
   afterEach(() => {
     jest.restoreAllMocks();
+  });
+
+  describe('update course publish state', () => {
+    it('course open (jex) and unpublished (canvas) => update to publish', async () => {
+      const now = '2020-01-01T00:00:00.000-05:00';
+
+      // course is open according to jex
+      jest.spyOn(jex, 'getActiveCourses');
+      jex.getActiveCourses.mockResolvedValue([
+        {
+          id: 'FDN-1111-07-F20',
+          name: 'Foundation: 2D -- G. Costanza (Sect. 07 - Fall 2020)',
+          courseCode: 'FDN  1111 07',
+          parentCourseCode: 'FDN  1111 07',
+          term: 'FA',
+          year: 2020,
+          startDate: '2020-01-02',
+          endDate: '2020-12-11',
+          openDate: '2020-01-01T00:00:00.000-05:00',
+          closeDate: '2020-12-31T23:59:59.000-06:00',
+          courseFormat: 'on_campus',
+          instructor: { id: 114, firstName: 'George', lastName: 'Costanza' },
+        },
+      ]);
+
+      // course is unpublished in Canvas
+      jest.spyOn(canvas, 'getCourses');
+      canvas.getCourses.mockResolvedValue([
+        {
+          id: 2,
+          name: 'Foundation: 2D -- G. Costanza (Sect. 07 - Fall 2020)',
+          start_at: '2020-01-01T00:00:00.000-05:00',
+          end_at: '2020-12-31T23:59:59.000-06:00',
+          course_code: 'FDN-1111-07-F20',
+          sis_course_id: 'FDN-1111-07-F20',
+          sis_import_id: null,
+          workflow_state: 'unpublished', // should be published
+          course_format: 'on_campus',
+        },
+      ]);
+
+      const csv = await generateCourseUpdates({ today: now });
+      expect(csv).toMatchInlineSnapshot(`
+        "course_id,short_name,long_name,status,start_date,end_date
+        FDN-1111-07-F20,FDN-1111-07-F20,Foundation: 2D -- G. Costanza (Sect. 07 - Fall 2020),published,2020-01-01T00:00:00.000-05:00,2020-12-31T23:59:59.000-06:00"
+      `);
+    });
+
+    it('course open (jex) and published (canvas) => no action taken', async () => {
+      const now = '2020-01-01T00:00:00.000-05:00';
+
+      // course is OPEN according to jex
+      jest.spyOn(jex, 'getActiveCourses');
+      jex.getActiveCourses.mockResolvedValue([
+        {
+          id: 'FDN-1111-07-F20',
+          name: 'Foundation: 2D -- G. Costanza (Sect. 07 - Fall 2020)',
+          courseCode: 'FDN  1111 07',
+          parentCourseCode: 'FDN  1111 07',
+          term: 'FA',
+          year: 2020,
+          startDate: '2020-01-02',
+          endDate: '2020-12-11',
+          openDate: '2020-01-01T00:00:00.000-05:00',
+          closeDate: '2020-12-31T23:59:59.000-06:00',
+          courseFormat: 'on_campus',
+          instructor: { id: 114, firstName: 'George', lastName: 'Costanza' },
+        },
+      ]);
+
+      // course is PUBLISHED in Canvas
+      jest.spyOn(canvas, 'getCourses');
+      canvas.getCourses.mockResolvedValue([
+        {
+          id: 2,
+          name: 'Foundation: 2D -- G. Costanza (Sect. 07 - Fall 2020)',
+          start_at: '2020-01-01T00:00:00.000-05:00',
+          end_at: '2020-12-31T23:59:59.000-06:00',
+          course_code: 'FDN-1111-07-F20',
+          sis_course_id: 'FDN-1111-07-F20',
+          sis_import_id: null,
+          workflow_state: 'published',
+          course_format: 'on_campus',
+        },
+      ]);
+
+      const csv = await generateCourseUpdates({ today: now });
+      expect(csv).toMatchInlineSnapshot(`""`);
+    });
+    it.todo('course not open (jex) => no change to publish state');
+    it.todo('after end date (jex) => no action taken');
+    it.todo('open (jex) and does not exist on canvas => ignore');
   });
 
   it.todo('lists courses with names that need updating');
