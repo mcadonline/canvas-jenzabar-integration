f92a31879a9bc47c0f74b0745f8a23f396bfbf49
Manish Sharma <arnoltherasing@gmail.com>
2022-10-12 14:05:22 -0400

    remove unwanted comments

--- courses/utils/db.js
+++ courses/utils/db.js
@@ -13,23 +13,4 @@ function initiateDb() {
     require('../migrations')(db.connection);
 }
 
-
-// db.serialize(() => {
-//   db.each(`SELECT PlaylistId as id,
-//                   Name as name
-//            FROM playlists`, (err, row) => {
-//     if (err) {
-//       console.error(err.message);
-//     }
-//     console.log(row.id + "\t" + row.name);
-//   });
-// });
-
-// db.close((err) => {
-//   if (err) {
-//     console.error(err.message);
-//   }
-//   console.log('Close the database connection.');
-// });
-
 module.exports = { initiateDb, db };
