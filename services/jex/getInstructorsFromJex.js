211da7833a93f3979484b1f9f01be2a5ec4075a8
Manish Sharma <arnoltherasing@gmail.com>
2023-02-02 16:28:59 -0500

    add unit test cases

--- services/jex/getInstructorsFromJex.js
+++ services/jex/getInstructorsFromJex.js
@@ -1,3 +1,4 @@
+import { NAME_MASTER, CONTACT_TABLE, USERNAME_TABLE } from '../../constants/jex.js';
 import jexService from './jexService.js';
 
 const sqlQuery = `
@@ -11,16 +12,16 @@ select distinct nm.ID_NUM as id
     , rtrim(am_peml.AlternateContact) as personalEmail
     , rtrim(am_meml.AlternateContact) as mcadEmail
     , rtrim(nmu.mcad_username) as username
-from nameMaster nm
+from ${NAME_MASTER} nm
 join section_master sm
     on sm.LEAD_INSTRUCTR_ID = nm.id_num
-left join AlternateContactMethod am_peml
+left join ${CONTACT_TABLE} am_peml
     on nm.id_num = am_peml.id_num
     and am_peml.addr_cde = 'PEML'
-left join AlternateContactMethod am_meml
+left join ${CONTACT_TABLE} am_meml
     on nm.id_num = am_meml.id_num
     and am_meml.addr_cde = 'MEML'
-left join name_master_udf nmu
+left join ${USERNAME_TABLE} nmu
     on nm.id_num = nmu.id_num
 where sm.LAST_END_DTE >= @today
 `;
